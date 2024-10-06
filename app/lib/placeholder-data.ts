6482eb241ac923fc6461dc9459184559a28a9cdb
tomisin <tomjames156@gmail.com>
2024-10-06 13:47:06 +0100

    added source files

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,35 @@
+const workExperiences = [
+    {
+        exp_id: 0,
+        organisation: "Nile University",
+        role: "Software Engineering Student",
+        duration: "September 2022 - Present",
+        experiences: [
+            "Lorem ipsum dolor sit amet consectetur. Viverra odio orci tellus ornare blandit. Eros nisl vulputate suscipit lobortis lobortis sed ut. Mauris arcu diam ridiculus ut consectetur risus ornare posuere. Proin commodo dui ultricies senectus aliquam.",
+            "Lorem ipsum dolor sit amet consectetur. Viverra odio orci tellus ornare blandit. Eros nisl vulputate suscipit lobortis lobortis."
+        ]
+
+    },
+    {
+        exp_id: 1,
+        organisation: "ChitHub",
+        role: "Data Science Intern",
+        duration: "July 2024 - Present",
+        experiences: [
+            "Lorem ipsum dolor sit amet consectetur. Viverra odio orci tellus ornare blandit. Eros nisl vulputate suscipit lobortis lobortis sed ut. Mauris arcu diam ridiculus ut consectetur risus ornare posuere. Proin commodo dui ultricies senectus aliquam.",
+            "Lorem ipsum dolor sit amet consectetur. Viverra odio orci tellus ornare blandit. Eros nisl vulputate suscipit lobortis lobortis."
+        ]
+
+    },
+    {
+        exp_id: 2,
+        organisation: "Medbolt",
+        role: "Frontend Engineer",
+        duration: "December 2022 - December 2023",
+        experiences: [
+            "Lorem ipsum dolor sit amet consectetur. Viverra odio orci tellus ornare blandit. Eros nisl vulputate suscipit lobortis lobortis sed ut. Mauris arcu diam ridiculus ut consectetur risus ornare posuere. Proin commodo dui ultricies senectus aliquam.",
+            "Lorem ipsum dolor sit amet consectetur. Viverra odio orci tellus ornare blandit. Eros nisl vulputate suscipit lobortis lobortis."
+        ]
+
+    }
+]
