bbe5079917b7f20fdb3531e7e2620a198824e183
tomisin <tomjames156@gmail.com>
2025-02-19 08:29:04 +0100

    added resume

--- app/ui/Hero.tsx
+++ app/ui/Hero.tsx
@@ -1,4 +1,5 @@
 import { libre_franklin } from './fonts'
+import Link from 'next/link'
 
 export default function Hero(){
     return (
@@ -7,13 +8,13 @@ export default function Hero(){
                 <p className='text-9xl wavehand'>👋🏾</p>
                 <h1 className='text-4xl mt-10 font-bold'>Hi there, I’m Tomisin</h1>
                 <h3 className='text-2xl mt-4 font-medium'>I love writing code</h3>
-                <p className={`mt-4 leading-[1.4rem] text-[1.1rem] ${libre_franklin.className} antialiased`}>I’m a fullstack software engineer and software engineering student from Abuja, Nigeria. I’m passionate about building innovative software based solutions to real-world problems.</p>
-                <button title="Download Resume" className='mt-10 mx-auto flex border-[0.1rem] border-blue-900 text-center align-center text-blue-900 pl-2 pr-4 py-0.5 rounded-md gap-[0.25rem] transition-all ease-in-out duration-100 hover:bg-blue-900 hover:text-white hover:border-[0.15rem]'>
+                <p className={`mt-4 leading-[1.4rem] text-[1.1rem] ${libre_franklin.className} antialiased`}>I’m a budding data scientist and and software engineering student from Abuja, Nigeria. I’m passionate about building innovative software based solutions to real-world problems.</p>
+                <Link href="/resume.pdf" target='_blank' download={true} title="Download Resume" className='w-fit mt-10 mx-auto flex border-[0.1rem] border-blue-900 text-center align-center text-blue-900 pl-2 pr-4 py-0.5 rounded-md gap-[0.25rem] transition-all ease-in-out duration-100 hover:bg-blue-900 hover:text-white hover:border-[0.15rem]'>
                     <span className="material-symbols-rounded hover:[--font-FILL:1] hover:[--font-wght:600]">
                         download
                     </span>  
                     Resume       
-                </button>
+                </Link>
             </div>
         </section>
     )
--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -6,56 +6,53 @@ import linkedin from "../../public/linkedin.png"
 
 export default function Navbar(){
     return (
-        <nav className="flex justify-between align-center bg-white px-12 py-4">
+        <nav className="flex justify-between align-center bg-white px-12 py-4 fixed left-0 top-0 w-full z-20">
             <div className="flex-2 flex items-center gap-3">
-                <Link href='/' className="font-bold text-[1.2rem]">tom1s1n</Link>
+                <Link href='/' className="font-bold text-[1.2rem] hover:text-gray-500 transition transition-all duration-200">tom1s1n</Link>
                 <div>                
                     <div className="flex items-center justify-center gap-4">
-                        <Link 
+                        <Link
                             href="mailto:tomjames156@gmail.com"
                         >
-                            <div>
-                                <Image
-                                    src={gmail}
-                                    alt='Gmail Logo'
-                                    width={25}
-                                    height={25}
-                                />
-                            </div>
+                            <Image 
+                                className="hover:-mt-2 transition transition-all duration-200"
+                                src={gmail}
+                                alt='Gmail Logo'
+                                width={25}
+                                height={25}
+                            />
                         </Link>
                         <Link 
                             href="https://github.com/tomjames156/"
                             target="_blank"
                         >
-                            <div>
-                                <Image
-                                    src={github}
-                                    alt='Github Logo'
-                                    width={20}
-                                    height={20}
-                                />
-                            </div>
+                            <Image
+                                className="hover:-mt-2 transition transition-all duration-200"
+                                src={github}
+                                alt='Github Logo'
+                                width={20}
+                                height={20}
+                            />
                         </Link>
                         <Link 
                             href="https://www.linkedin.com/in/tomisin-akinwande-981842247/"
                             target="_blank"
                         >
-                            <div>
-                                <Image
-                                    src={linkedin}
-                                    alt='Linkedin logo'
-                                    width={18}
-                                    height={18}
-                                />
-                            </div>
+                            <Image
+                                className="hover:-mt-2 transition transition-all duration-200"
+                                src={linkedin}
+                                alt='Linkedin logo'
+                                width={18}
+                                height={18}
+                            />
                         </Link>
                     </div>
                 </div>
             </div>
             <div className="flex-2 flex gap-12">
-                <Link href='#about' className="font-medium">About</Link>
-                <Link href='#projects' className="font-medium">Projects</Link>
-                <Link href='#experience' className="font-medium">Experience</Link>
+                <Link href='#about' className="font-medium hover:font-bold transition transition-all duration-200">About</Link>
+                <Link href='#projects' className="font-medium hover:font-bold transition transition-all duration-200">Projects</Link>
+                <Link href='#experience' className="font-medium hover:font-bold transition transition-all duration-200">Experience</Link>
             </div>
         </nav>
     )
