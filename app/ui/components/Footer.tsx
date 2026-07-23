c52066f752809ba037e06656282a015cf4789385
tomjames156 <tomjames156@gmail.com>
2026-07-23 23:11:18 +0100

    updated the footer date

--- app/ui/components/Footer.tsx
+++ app/ui/components/Footer.tsx
@@ -2,6 +2,6 @@ import { libre_franklin } from "../fonts"
 
 export default function Footer (){
     return (<footer className="mt-32 pb-6 pt-6 border border-[#ffffff25] !border-t-[1px] border-r-0 border-b-0 border-l-0">
-        <p className={`${libre_franklin.className} antialiased text-center text-xs dark:text-white `}>Developed by Tomisin Akinwande <br/>&copy; 2025</p>
+        <p className={`${libre_franklin.className} antialiased text-center text-xs dark:text-white `}>Developed by Tomisin Akinwande<br/>&copy; 2026</p>
     </footer>)
 }
